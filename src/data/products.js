c2d4eb65d603dbbcc5c6c4141e12d512d2bf6426
juanjzb <zb.juanjose@gmail.com>
2025-09-07 09:34:23 -0600

    feat: add new products and change sections order

---
+++ src/data/products.js
@@ -0,0 +1,116 @@
+export const products = [
+  {
+    name: 'Tomate',
+    image: 'tomate.jpg',
+    price: 'C$ 18 x Libra',
+    available: true
+  },
+  {
+    name: 'Cebolla',
+    image: 'cebolla.jpg',
+    price: 'C$ 25 x Libra',
+    available: true
+  },
+  {
+    name: 'Zanahoria',
+    image: 'zanahoria.jpg',
+    price: 'C$ 25 x Libra',
+    available: true
+  },
+  {
+    name: 'Chiltoma',
+    image: 'chiltoma.jpg',
+    price: 'C$ 7 x Unidad',
+    available: true
+  },
+  {
+    name: 'Ajo',
+    image: 'ajo.jpg',
+    price: 'C$ 7 x Unidad',
+    available: true
+  },
+  {
+    name: 'Yuca',
+    image: 'yuca.jpeg',
+    price: 'C$ 15 x Libra',
+    available: true
+  },
+  {
+    name: 'Malanga',
+    image: 'malanga.jpg',
+    price: 'C$ 15 x Libra',
+    available: true
+  },
+  {
+    name: 'Platano',
+    image: 'platano.jpg',
+    price: 'C$ 10 y 15 x Unidad',
+    available: true
+  },
+  {
+    name: 'Chaya',
+    image: 'chaya.jpg',
+    price: 'C$ 15 x Unidad',
+    available: true
+  },
+  {
+    name: 'Repollo',
+    image: 'repollo.jpg',
+    price: 'C$ 50 x Unidad',
+    available: true
+  },
+  {
+    name: 'Ayote',
+    image: 'ayote.webp',
+    price: 'C$ 30 x Unidad',
+    available: false
+  },
+  {
+    name: 'Naranja p. Fresco',
+    image: 'naranjas.jpg',
+    price: 'C$ 30 x Docena',
+    available: true
+  },
+  {
+    name: 'Limon Mandarina',
+    image: 'limon-mandarina.jpeg',
+    price: 'C$ 30 x Docena',
+    available: true
+  },
+  {
+    name: 'Aguacate',
+    image: 'aguacate.webp',
+    price: 'C$ 10',
+    available: false
+  },
+  {
+    name: 'Banano Blanco',
+    image: 'banano-blanco.avif',
+    price: 'C$ 40 x Docena',
+    available: true
+  },
+  {
+    name: 'Guineo Cuadrado',
+    image: 'guineo-cuadrado.jpeg',
+    price: 'C$ 30 x Docena',
+    available: true
+  },
+  {
+    name: 'Pepino',
+    image: 'pepino.jpeg',
+    price: 'C$ 10 x Unidad',
+    available: true
+  },
+  {
+    name: 'Sandia',
+    image: 'sandia.jpg',
+    price: 'C$ 50 x Unidad',
+    available: true
+  },
+  {
+    name: 'Naranja Agria',
+    image: 'naranja-agria.jpg',
+    price: 'C$ 100 x Docena',
+    available: true
+  }
+]
